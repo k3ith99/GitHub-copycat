944d65e5dca27f266342f63f9fec49834f07f135
jalexxx <jumaanahalex@gmail.com>
2022-03-09 17:14:06 +0000

    made the homepage with image and commented out nav

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,7 +7,7 @@ function Home() {
   return (
     <>
     <div id="welcome_div">
-      <h1> Hello! </h1>
+      <h1> Hello there stranger! </h1>
       <h2> we welcome you to this site, where you able to steathily stalk people on github 
         without actually going on github. Use us to your hearts extent, we wont shame you nor 
         be keeping an eye on you...or are we? </h2>
