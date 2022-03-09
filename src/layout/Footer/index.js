3f6ea48d621fb94f0361255c5e0a9e9e8ce33034
jalexxx <jumaanahalex@gmail.com>
2022-03-09 21:03:24 +0000

    fiixng the footer

--- src/layout/Footer/index.js
+++ src/layout/Footer/index.js
@@ -5,10 +5,13 @@ const Footer = () => {
   return (
     <>
       <div id="footer">
+        
       <a href="https://github.com/k3ith99/GitHub-copycat" >
    <img src="https://img.icons8.com/color/96/000000/my-melody.png" />
       </a>
+      <h3> ©cutipihub </h3>
       </div>
+
     </>
   );
 };
