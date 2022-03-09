732ac6aa59c376bdae958a0afce4e7c69a192bea
jalexxx <jumaanahalex@gmail.com>
2022-03-09 16:24:28 +0000

    made a welcome div

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,10 +6,11 @@ function Home() {
 
   return (
     <>
-    <div>
+    <div id="welcome_div">
       <h1> Hello! </h1>
       <h2> we welcome you to this site, where you able to steathily stalk people on github 
-        without actually going on github.</h2>
+        without actually going on github. Use us to your hearts extent, we wont shame you nor 
+        be keeping an eye on you...or are we? </h2>
         </div>
     </>
   );
