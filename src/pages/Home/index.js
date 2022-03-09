9efd1012d480835c3c6cf888cc3d81e8c1c35b68
jalexxx <jumaanahalex@gmail.com>
2022-03-09 16:22:35 +0000

    changed welcome text

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,8 +6,11 @@ function Home() {
 
   return (
     <>
-      <h1>WELCOME</h1>
-
+    <div>
+      <h1> Hello! </h1>
+      <h2> we welcome you to this site, where you able to steathily stalk people on github 
+        without actually going on github.</h2>
+        </div>
     </>
   );
 }
